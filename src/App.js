85b41523f9a3d974dd02f509ce7e813c68b03a64
Mike Le <mike.le@live.com>
2021-03-09 23:04:24 -0800

    completed assignment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [ pokemonList, setPokemonList ] = useState([]);
+
+  const clickHandler = (e) => {
+    fetch('https://pokeapi.co/api/v2/pokemon/?limit=802')
+    .then( response =>  response.json() )
+    .then( response => {
+      setPokemonList( response.results );
+    } )
+  };  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={ clickHandler }>Fetch Pokemon</button>
+      <ul>
+      { pokemonList 
+          ? pokemonList.map( (pokemon, index) => {
+            return <li key={ index }>{ pokemon.name }</li>
+          } )
+        : null
+      } 
+      </ul>
     </div>
   );
 }
